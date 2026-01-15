ba953105bace0b8f50f51ef8370da346b1910b0b
marcel <marcel.hajek7@gmail.com>
2026-01-15 23:14:04 +0100

    ...

---
+++ player/Public/player/Player.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "Object.h"
+
+namespace player {
+using object::Object;
+
+class Player final : public Object {
+  public:
+    void emerge() override {}
+    void tick() override {}
+};
+} // namespace player
---
+++ world/Public/world/World.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "Object.h"
+#include <memory>
+#include <vector>
+
+namespace object {
+class Object;
+}
+namespace player {
+class Player;
+}
+
+namespace world {
+using object::Object;
+using player::Player;
+
+class World final {
+  public:
+    World() {}
+
+    World(const World &) = delete;
+    World &operator=(const World &) = delete;
+    World(World &&) = delete;
+    World &operator=(World &&) = delete;
+    ~World() = default;
+
+    [[nodiscard]] World &
+    addObject(std::shared_ptr<Object> &&object);
+
+    void emerge();
+
+  private:
+    void tick();
+
+    std::vector<std::shared_ptr<Object>> objects_;
+};
+} // namespace world
