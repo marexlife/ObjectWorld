ce55d800c6b67bac824c1b5b18fd0f58eefcf3a5
marcel <marcel.hajek7@gmail.com>
2026-01-15 23:52:05 +0100

    ...

--- main/Private/main.cpp
+++ main/Private/main.cpp
@@ -1,10 +1,6 @@
 #include "Player.h"
 #include "World.h"
-#include <memory>
 
 int main() {
-    auto p1 = std::make_shared<Player>();
-    auto p2 = std::make_shared<Player>();
-
     World().addObject<Player>().emerge();
 }
