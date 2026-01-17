7eaeb18b5fbd481f214dd54ee85769d672631e99
marcel <marcel@computer.fritz.box>
2026-01-17 23:24:57 +0100

    .

--- cmake/deps.cmake
+++ cmake/deps.cmake
@@ -1,8 +1,8 @@
-function(CUSTOM_DEPS CUSTOM_TARGET_DIR)
 execute_process(COMMAND
     git clone 
     https://github.com/wxWidgets/wxWidgets.git 
     --branch v3.3.1 
     ${CUSTOM_TARGET_DIR}
 )
-endfunction()
+
+include_directories(third_party/wxWidgets/include)
