b7e552083fea2b33d42c1923376bbd17a168d51a
marcel <marcel@fedora-2.fritz.box>
2026-01-17 05:53:10 +0100

    .

--- player/Public/player/Player.h
+++ player/Public/player/Player.h
@@ -4,20 +4,14 @@
 #include <print>
 #include <string_view>
 
-class Player final : public Name, public Events {
+class Player final : public Events {
   public:
     ~Player() override {}
 
   private:
     /// runs once at emerging of the object
-    void emerge() override {
-        std::println("{}: emerge!", getObjectName());
-    }
+    void emerge() override {}
 
     /// runs once per frame
     void tick() override {}
-
-    std::string_view getObjectName() const override {
-        return "Player";
-    }
 };
