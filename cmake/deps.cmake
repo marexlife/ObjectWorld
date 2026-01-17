28325197f369c0e47670a9678e159de5105cb53a
marcel <marcel@computer.fritz.box>
2026-01-18 00:02:39 +0100

    .

--- cmake/deps.cmake
+++
@@ -1,8 +0,0 @@
-execute_process(COMMAND
-    git clone 
-    https://github.com/wxWidgets/wxWidgets.git 
-    --branch v3.3.1 
-    ${CUSTOM_TARGET_DIR}
-)
-
-include_directories(third_party/wxWidgets/include)
